dde9b261795a5c8fff3f6c8f3c85cb967afeea02
GaryGaster <gastergary.com>
2020-11-21 17:57:15 +0100

    Change databese to postgres

--- redecoration/settings.py
+++ redecoration/settings.py
@@ -79,10 +79,15 @@ WSGI_APPLICATION = 'redecoration.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'exampledb',
+        'USER': 'postgres',
+        'PASSWORD': 'example',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
